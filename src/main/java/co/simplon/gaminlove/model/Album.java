86ff7ced691d33b8125974982efa026cc985c27b
Maureen <maureen.defoort.lp4@gmail.com>
2020-01-22 12:09:03 +0100

    add album model

--- src/main/java/co/simplon/gaminlove/model/Album.java
+++ src/main/java/co/simplon/gaminlove/model/Album.java
@@ -1,5 +1,29 @@
 package co.simplon.gaminlove.model;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Album {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private Collection<Photo> photos;
 
 }
