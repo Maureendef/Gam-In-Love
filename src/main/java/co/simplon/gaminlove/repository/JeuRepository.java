eda8732a449529a19bf7a23e07af31a85198d704
Maureen <maureen.defoort.lp4@gmail.com>
2020-01-22 11:24:10 +0100

    add JeuController & update JeuRepository

--- src/main/java/co/simplon/gaminlove/repository/JeuRepository.java
+++ src/main/java/co/simplon/gaminlove/repository/JeuRepository.java
@@ -1,5 +1,7 @@
 package co.simplon.gaminlove.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.simplon.gaminlove.model.Jeu;
@@ -12,5 +14,11 @@ import co.simplon.gaminlove.model.Jeu;
  *
  */
 public interface JeuRepository extends CrudRepository<Jeu, Integer> {
+	// on peut générer automatiquement des méthodes de recherche dans le repository
+    // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+	public Optional<Jeu> findByNom(String nom);
+	public Optional<Jeu> deleteByNom(String nom);
 
+	
 }
+
