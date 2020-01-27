daf8c0b6a4d23956869c03941f15905113afc599
wukkox <56165426+wukkox@users.noreply.github.com>
2020-01-27 21:50:55 +0100

    Add files via upload

--- src/main/java/co/simplon/gaminlove/GaminloveApplication.java
+++ src/main/java/co/simplon/gaminlove/GaminloveApplication.java
@@ -3,7 +3,10 @@ package co.simplon.gaminlove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class GaminloveApplication {
 
 	public static void main(String[] args) {
